510a077c7edf77dcc8f7f1ef3c3b198bc6efb80c
Unknown <chaos777@inbox.ru>
2018-06-22 20:13:31 +0300

    correct listening message
    correct listening message

--- server/index.js
+++ server/index.js
@@ -12,4 +12,4 @@ app.get('/boards', (req, res) => {
   res.send('Hello world!');
 });
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+app.listen(3000, () => console.log('Server listening on port 3000!'));
