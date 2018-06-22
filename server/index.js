ffcc928c719dabbb538c2fe01d0c50a99dc19ced
Unknown <chaos777@inbox.ru>
2018-06-22 19:36:39 +0300

    add express
    add express & get

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+
+app.use(bodyParser.json());
+
+app.get('/boards', (req, res) => {
+  res.send('Hello world!');
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
