114a200ab9a925686beea185e99128ba903475bd
Unknown <max777.lebed@yandex.ru>
2018-06-22 12:52:37 +0300

    mongo

---
+++ server/db.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/BoardsDB');
+
+
+const BoardName = mongoose.model('BoardName', {
+    type: String,
+    BoardName: {
+        type: String,
+        default: ''
+    },
+
+    ItemName: String
+})
+
+
+module.exports = {
+    BoardName: BoardName
+}
