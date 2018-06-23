03efca0a30762c75bca5052ebc20420787f93ecb
Unknown <max777.lebed@yandex.ru>
2018-06-23 16:00:01 +0300

    DeleteBoard

--- server/index.js
+++ server/index.js
@@ -4,13 +4,21 @@ const app = express();
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
-  extended: false
+    extended: false
 }));
 
 app.use(bodyParser.json());
 
 app.get('/boards', (req, res) => {
-  res.send('Hello world!');
+    res.send('Hello world!');
 });
+
+function deleteBoard() {
+    app.delete('/:id', function(req, res, ) {
+        db.Board.deleteOne({ _id: { $in: req.params.id } }).then(() => {
+            res.send(' Deleted ');
+        })
+    })
+}
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
