53ab6f0525f0487f8c6dade97db88f5b0f3b1ec8
MaximSwan <40002859+MaximSwan@users.noreply.github.com>
2018-07-04 02:11:02 +0300

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
-let promMongo = new Promise((resolve, reject) => {
-    resolve(mongoose.connect('mongodb://localhost/boards'));
-});
-promMongo.then(
-    result => console.log('Successful connection'),
-    error => console.log("Can't connect to MongoDb")
-)
+
+mongoose.connect('mongodb://localhost/boards').then(
+result => console.log('Successful connection'),
+err => console.log("Can't connect to MongoDb")
+);
 
 const boardItemSchema = new mongoose.Schema({
     name: {
