b7c598ebf7255f84b19e3cbfb741ca798bf7754b
Unknown <max777.lebed@yandex.ru>
2018-07-12 19:25:22 +0300

    AddBoard

--- client/Angular/src/app/board/board.component.ts
+++ client/Angular/src/app/board/board.component.ts
@@ -11,6 +11,24 @@ export class boardComponent implements OnInit {
   @Input('isBoard') board:Board;
   constructor(private boardsService: BoardsService) { }
 
+  removeBoard(i, board:Board) {
+    
+    this.boardsService.saveDeleteBoard(i, board.id );
+    
+  }
+
+  ItemAdd(id, board) {
+    if (board.NewItem == 0) {
+      return;
+    }
+    let item = new Boarditem;
+    item.text = board.NewItem;
+    board.items.push(item); 
+    this.boardsService.AddItemNew(board.id, board);
+    board.NewItem = '';
+    console.log(board);
+  }
+  
   ngOnInit() {
   }
 }
